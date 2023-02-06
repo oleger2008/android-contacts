86afe6cc7f1649b98c084344a0e9513703018139
Oleg Tsoy <otsoy1995@yandex.ru>
2023-02-06 22:28:49 +0300

    fix extending generic interface

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -4,7 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DiffUtil;
 
-public class BaseListDiffCallback<T extends ListDiffCallback> extends DiffUtil.ItemCallback<T> {
+public class BaseListDiffCallback<T extends ListDiffCallback<T>> extends DiffUtil.ItemCallback<T> {
     @Override
     public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
         return oldItem.theSameAs(newItem);
